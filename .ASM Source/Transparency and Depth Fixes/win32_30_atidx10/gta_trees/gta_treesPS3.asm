09537db640667ac286270d819d9cb7ace4e9ad9a
Parallellines <133058741+Parallellines0451@users.noreply.github.com>
2023-08-26 04:06:13 -0300

    Restored 1.0.4.0 transparency

--- .ASM Source/Transparency and Depth Fixes/win32_30_atidx10/gta_trees/gta_treesPS3.asm
+++ .ASM Source/Transparency and Depth Fixes/win32_30_atidx10/gta_trees/gta_treesPS3.asm
@@ -5,45 +5,39 @@
 //
 //   sampler2D StippleTexture;
 //   sampler2D TextureSampler;
-//   float4 globalScalars;
 //
 //
 // Registers:
 //
 //   Name           Reg   Size
 //   -------------- ----- ----
-//   globalScalars  c39      1
 //   TextureSampler s0       1
 //   StippleTexture s10      1
 //
 
     ps_3_0
-    def c0, 3.99600005, 4, 0.125, 0.25
-    def c1, -1, -0, 0.5, 0
+    def c0, 0.125, 0.25, 0, 0
+    def c1, 1, 0, 3.99600005, 4
     dcl_texcoord v0.xy
-    dcl_texcoord1 v1.xyz
     dcl vPos.xy
     dcl_2d s0
     dcl_2d s10
-    texld r0, v0, s0
-    mul r0.w, r0.w, c39.x
+    mul r0, c1.xxyy, v0.xyxx
+    texldl r0, r0, s0
     mov_sat r1.x, r0.w
-    mul r1.x, r1.x, c0.x
+    mul r1.x, r1.x, c1.z
     frc r1.y, r1.x
-    mul r1.z, r1.y, c0.y
+    mul r1.z, r1.y, c1.w
     frc r1.w, r1.z
     add r2.xy, r1.zxzw, -r1.wyzw
-    mul r1.xy, c0.z, vPos
+    mul r1.xy, c0.x, vPos
     frc r1.xy, r1_abs
     cmp r1.xy, vPos, r1, -r1
-    mul r1.xy, r1, c0.w
-    mad r1.xy, r2, c0.w, r1
+    mul r1.xy, r1, c0.y
+    mad r1.xy, r2, c0.y, r1
     texld r1, r1, s10
-    cmp r1, -r1.y, c1.x, c1.y
+    cmp r1, -r1.y, -c1.x, -c1.y
     texkill r1
     mov oC0, r0
-    mad oC1, v1.xyzx, c1.zzzw, c1.zzzw
-    mov oC2, c1.wzzw
-    mov oC3, c1.w
 
-// approximately 20 instruction slots used (2 texture, 18 arithmetic)
+// approximately 18 instruction slots used (3 texture, 15 arithmetic)
