f6b587a016e74aa06fc5d52c91480e0e28cb07b7
Parallellines <133058741+Parallellines0451@users.noreply.github.com>
2023-09-02 14:13:06 -0300

    Update buildshaders.bat

--- buildshaders.bat
+++ buildshaders.bat
@@ -1,8 +1,8 @@
 @echo on
-set shaders_path=%cd%/.ASM Source
-set win32_30=%cd%/common/shaders/win32_30
-set win32_30_atidx10=%cd%/common/shaders/win32_30_atidx10
-set win32_30_nv8=%cd%/common/shaders/win32_30_nv8
+set shaders_path=%cd%/win32_30_nv8
+set win32_30=%cd%/.Build/common/shaders/win32_30
+set win32_30_atidx10=%cd%/.Build/common/shaders/win32_30_atidx10
+set win32_30_nv8=%cd%/.Build/common/shaders/win32_30_nv8
 
 for /R "%shaders_path%" %%a in (*.xml) do (
     RageShaderEditor.exe "%%a"
@@ -14,3 +14,6 @@ echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y
 echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y
 del "%%i"
 )
+
+echo D | xcopy "%cd%\other_assets\Improved Stipple" "%cd%\.Build" /K /H /Y /S
+echo D | xcopy "%cd%\other_assets\Opaque Wires" "%cd%\.Build" /K /H /Y /S
