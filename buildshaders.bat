ffec39e05a1c9f67e81f7b30daa54a4313eb36f7
Parallellines <133058741+Parallellines0451@users.noreply.github.com>
2023-10-02 20:40:40 -0300

    Removed gta_trees_new

--- buildshaders.bat
+++ buildshaders.bat
@@ -1,8 +1,8 @@
 @echo on
 set shaders_path=%cd%/win32_30_nv8
-set win32_30=%cd%/.Build/common/shaders/win32_30
-set win32_30_atidx10=%cd%/.Build/common/shaders/win32_30_atidx10
-set win32_30_nv8=%cd%/.Build/common/shaders/win32_30_nv8
+set win32_30=%cd%/update/common/shaders/win32_30
+set win32_30_atidx10=%cd%/update/common/shaders/win32_30_atidx10
+set win32_30_nv8=%cd%/update/common/shaders/win32_30_nv8
 
 for /R "%shaders_path%" %%a in (*.xml) do (
     RageShaderEditor.exe "%%a"
@@ -10,10 +10,7 @@ for /R "%shaders_path%" %%a in (*.xml) do (
 
 for /R "%shaders_path%" %%i in (*.fxc) do (
 echo D | xcopy "%%i" "%win32_30%" /K /H /Y
-echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y
-echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y
 del "%%i"
 )
 
-echo D | xcopy "%cd%\other_assets\Improved Stipple" "%cd%\.Build" /K /H /Y /S
-echo D | xcopy "%cd%\other_assets\gta_trees_new dependencies" "%cd%\.Build" /K /H /Y /S
+echo D | xcopy "%cd%/other_assets/Improved Stipple" "%cd%/update" /K /H /Y /S
