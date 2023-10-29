89c47014e9f27016a186fcdc5934fdf3eeb7887a
Parallellines <133058741+Parallellines0451@users.noreply.github.com>
2023-10-29 18:08:39 -0300

    Fix more stencils

--- win32_30_nv8/gta_trees/gta_treesPS1.asm
+++ win32_30_nv8/gta_trees/gta_treesPS1.asm
@@ -21,7 +21,7 @@
 //
 
     ps_3_0
-    def c151, 0.9999, 0.0234375, 0.0313725490196078, 0	// Dithering Mask constants
+    def c151, 0.9999, 0.0234375, 0.02734375, 0	// Dithering Mask constants
     def c219, 1.8395173895e+25, 3.9938258725e+24, 4.5435787456e+30, 8.5619336170e-43 // 611
     def c150, 15.996, 16, 0.0625, 0.0625	// 256 state stipple constants
     def c127, 0.9999999, 1, 0, 0	// LogDepth constants
--- win32_30_nv8/rage_billboard_nobump/rage_billboard_nobumpPS1.asm
+++ win32_30_nv8/rage_billboard_nobump/rage_billboard_nobumpPS1.asm
@@ -18,7 +18,7 @@
 //
 
     ps_3_0
-    def c151, 0.9999, 0.01171875, 0.0313725490196078, 0	// Dithering Mask constants
+    def c151, 0.9999, 0.01171875, 0.02734375, 0	// Dithering Mask constants
     def c219, 1.8395173895e+25, 3.9938258725e+24, 4.5435787456e+30, 1.0846050114e-42 // 774
     def c150, 15.996, 16, 0.0625, 0.0625	// 256 state stipple constants
     def c127, 0.9999999, 1, 0, 0	// LogDepth constants
