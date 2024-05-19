43a916369c2378de819cc36d4539dd9af881fe6d
Parallellines <133058741+Parallellines0451@users.noreply.github.com>
2024-05-18 23:00:30 -0300

    Create Receiver Plane Depth Bias.asm

---
+++ other_assets/Code Snippets/Receiver Plane Depth Bias.asm
@@ -0,0 +1,11 @@
+	mov r22.xy, r0.zw // UV
+	mov r22.z, r1.z // light depth
+	dsx r20.xyz, r22.xyz
+	dsy r21.xyz, r22.xyz
+	
+	mul r22.xyz, r20.yzy, r21.zxx
+	mad r22.xyz, r21.yzy, r20.zxx, -r22.xyz
+	rcp r22.z, r22.z
+	mul r22.xy, r22.xy, r22.z
+	
+	// lightdepth += dot(r22.xy, offset.xy);
